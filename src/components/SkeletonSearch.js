ac27b1707f04bd97bd4ceddd46ce5d28d2f623ff
hidayatNgganteng <dayatznur@gmail.com>
2021-12-13 02:56:14 +0700

    - fix skeleton search for mobile

--- src/components/SkeletonSearch.js
+++ src/components/SkeletonSearch.js
@@ -3,8 +3,8 @@ import Skeleton from "@mui/material/Skeleton";
 
 const SkeletonSearch = () => {
   return (
-    <div className="w-full px-135px mt-55px flex flex-row flex-wrap justify-between">
-      <div className="w-30percent my-20px">
+    <div className="w-full px-20px lg:px-135px mt-55px flex flex-row flex-wrap justify-between">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
@@ -22,7 +22,7 @@ const SkeletonSearch = () => {
           width={"40%"}
         />
       </div>
-      <div className="w-30percent my-20px">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
@@ -40,7 +40,7 @@ const SkeletonSearch = () => {
           width={"40%"}
         />
       </div>
-      <div className="w-30percent my-20px">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
@@ -58,7 +58,7 @@ const SkeletonSearch = () => {
           width={"40%"}
         />
       </div>
-      <div className="w-30percent my-20px">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
@@ -76,7 +76,7 @@ const SkeletonSearch = () => {
           width={"40%"}
         />
       </div>
-      <div className="w-30percent my-20px">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
@@ -94,7 +94,7 @@ const SkeletonSearch = () => {
           width={"40%"}
         />
       </div>
-      <div className="w-30percent my-20px">
+      <div className="w-full lg:w-30percent my-20px">
         <Skeleton
           variant="rectangular"
           className="skeleton-background"
